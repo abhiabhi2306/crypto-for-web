778a2e0d7f50a48939691406f58e371a2e3c0d08
Abhishek Sidharth <32158780+abhiabhi2306@users.noreply.github.com>
2021-08-12 15:38:01 +0530

    Create jwt1.py

---
+++ web/jwt1.py
@@ -0,0 +1,4 @@
+import jwt
+enc = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGFnIjoiY3J5cHRve2p3dF9jb250ZW50c19jYW5fYmVfZWFzaWx5X3ZpZXdlZH0iLCJ1c2VyIjoiQ3J5cHRvIE1jSGFjayIsImV4cCI6MjAwNTAzMzQ5M30.shKSmZfgGVvd2OSB2CGezzJ3N6WAULo3w9zCl_T47KQ"
+secret = "secret"
+print(jwt.decode(enc,secret,algorithms=["HS256"],options={"verify_signature": False}))
